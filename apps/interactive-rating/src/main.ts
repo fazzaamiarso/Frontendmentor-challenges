77dda1590e677126d0641c5ce94ddbd80d23eb09
fazzaamiarso <fazzaamiarso@gmail.com>
2022-12-10 02:48:32 +0700

    fix split reading of VO

--- apps/interactive-rating/src/main.ts
+++ apps/interactive-rating/src/main.ts
@@ -3,11 +3,13 @@ const selectedRatingEl = document.querySelector(
   "#selected-rating"
 ) as HTMLSpanElement;
 
-const mainPage = document.getElementById("rate-page") as HTMLElement;
-const thankPage = document.getElementById("thank-page") as HTMLElement;
+const mainPage = document.getElementById("rate") as HTMLElement;
+const thankPage = document.getElementById("thank") as HTMLElement;
+const ratingStatus = document.getElementById("rating-status") as HTMLElement;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  e.stopPropagation();
   if (!(e.currentTarget instanceof HTMLFormElement)) return;
 
   const formData = new FormData(e.currentTarget);
@@ -15,12 +17,8 @@ form.addEventListener("submit", (e) => {
 
   mainPage.classList.remove("active");
   thankPage.classList.add("active");
-  const selectedRatingText = document.getElementById(
-    "rating-text"
-  ) as HTMLElement;
 
-  selectedRatingText.ariaLabel = `You selected ${selectedRatingEl.innerText} out of 5`;
-  selectedRatingText.focus();
+  ratingStatus.innerText = `You selected ${selectedRatingEl.innerText} out of 5`;
 });
 
 export {};
